8819668dfc53a85bfabab2fbe7e5ad040ce6b3b1
Ben Grinthal <bgrinthal@gmail.com>
2022-06-09 08:45:56 -0400

    added index file with prompts and questions

--- index.js
+++ index.js
@@ -1,16 +1,111 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of the project?',
+'What was your motivation for this projct?',
+'What problem does this solve?',
+'What did you learn?',
+'Provide any instalation instruction:',
+'Provide any usage instructions:',
+'Provide any contributing guidlines:',
+'Provide any test instructions:',
+'Enter GitHub username:',
+'Enter emal address:',
+'Choose a license'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const readmePageContent = generateHTML(data);
+
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  };
+    // Call fs write file function with passed file name and data as input arguments
+    // You can either use async call with callback function or synchronous call
+    // HINT: optional - use standard library function path to construct absolute path
+    //  to the current folder and file name
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // 1. Make a call to inquirer.prompt with passed questions as input argument
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'motivation',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'solve',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'learn',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'instalation',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'contribute',
+                message: questions[6],
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: questions[7],
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[8],
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[9],
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[10],
+                choices: [
+                    "Apache",
+                    "GNU",
+                    "ISC",
+                    "MIT",
+                    "Mozilla",
+                ]
+            },
+        ])
+    // 2. In .then callback, make a call to the utility function genereateMarkdown
+    //  with passed answers from user input as input argument and
+    //  store the return value to a data variable
+    .then((data) => {
+        generateMarkdown(data)
+    });
+    // 3. Make a call to writeToFile passed file name and data as input arguments
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,52 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // With template literal, create README file markdown file content and return it to the caller
+  //  using template literal variables to call helper functions above
+  //  variables inside template lierat string will be replaced with the return values from the helper functions
+
   return `# ${data.title}
+
+  ## Description
+  
+  - What was your motivation?
+  ${data.motivation}
+
+  - What problem does it solve?
+  ${data.solve}
+
+  - What did you learn?
+  ${data.learn}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  ${data.instalation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+  <br />
+  This application is covered by the ${answers.license} license. 
+  
+  ## How to Contribute
+  ${data.contribute}
+
+  ## Tests
+  ${data.test}
+
+  ## Any Questions?
+  -Pleas contact me through:
+  GitHub:   https://github.com/${data.github}
+  Email:   ${data.email}
+
 
 `;
 }
